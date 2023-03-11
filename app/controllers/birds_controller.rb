b50a884f1d3050e2e27ae704d62622de4ac90870
chengying <hollye2558@gmail.com>
2023-03-11 12:01:26 -0500

    completed

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,64 +1,121 @@
 class BirdsController < ApplicationController
 
-  # GET /birds
-  def index
-    birds = Bird.all
-    render json: birds
-  end
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
-  # POST /birds
-  def create
-    bird = Bird.create(bird_params)
-    render json: bird, status: :created
-  end
+ def index
+  birds = Bird.all
+  render json: birds
+ end
 
-  # GET /birds/:id
-  def show
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
-  end
+ def create
+  bird = Bird.create(bird_params)
+  render json: bird, status: :created
+ end
 
-  # PATCH /birds/:id
-  def update
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      bird.update(bird_params)
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
-  end
+ def show
+  bird = find_bird
+  render json: bird
+ end
 
-  # PATCH /birds/:id/like
-  def increment_likes
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      bird.update(likes: bird.likes + 1)
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
-  end
+ def update
+  bird = find_bird
+  bird.update(bird_params)
+  render json: bird
+ end
 
-  # DELETE /birds/:id
-  def destroy
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      bird.destroy
-      head :no_content
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
-  end
+def increment_likes
+  bird = find_bird
+  bird.update(likes: bird.likes + 1 )
+  render json: bird
+end
 
-  private
+def destroy
+  bird = find_bird
+  bird.destroy
+  head :no_content
+end
 
-  def bird_params
-    params.permit(:name, :species, :likes)
-  end
+ private
+
+ def bird_params
+  params.permit(:name, :species, :likes)
+ end
+
+ def find_bird
+  Bird.find(params[:id]) 
+ end
+
+ def render_not_found_response
+  render json: {error: "Bird not found"}, status: :not_found
+ end
+
+
+
+
+
+
+
+
+
+  # # GET /birds
+  # def index
+  #   birds = Bird.all
+  #   render json: birds
+  # end
+
+  # # POST /birds
+  # def create
+  #   bird = Bird.create(bird_params)
+  #   render json: bird, status: :created
+  # end
+
+  # # GET /birds/:id
+  # def show
+  #   bird = Bird.find_by(id: params[:id])
+  #   if bird
+  #     render json: bird
+  #   else
+  #     render json: { error: "Bird not found" }, status: :not_found
+  #   end
+  # end
+
+  # # PATCH /birds/:id
+  # def update
+  #   bird = Bird.find_by(id: params[:id])
+  #   if bird
+  #     bird.update(bird_params)
+  #     render json: bird
+  #   else
+  #     render json: { error: "Bird not found" }, status: :not_found
+  #   end
+  # end
+
+  # # PATCH /birds/:id/like
+  # def increment_likes
+  #   bird = Bird.find_by(id: params[:id])
+  #   if bird
+  #     bird.update(likes: bird.likes + 1)
+  #     render json: bird
+  #   else
+  #     render json: { error: "Bird not found" }, status: :not_found
+  #   end
+  # end
+
+  # # DELETE /birds/:id
+  # def destroy
+  #   bird = Bird.find_by(id: params[:id])
+  #   if bird
+  #     bird.destroy
+  #     head :no_content
+  #   else
+  #     render json: { error: "Bird not found" }, status: :not_found
+  #   end
+  # end
+
+  # private
+
+  # def bird_params
+  #   params.permit(:name, :species, :likes)
+  # end
 
 end
